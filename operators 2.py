7178822691ab771dbbd5fbc0f1c66fd79fa7592b
Sidney Kim <wz0627@gmail.com>
2018-04-03 14:06:03 +0900

    after lunch

---
+++ operators 2.py
@@ -0,0 +1,19 @@
+a = 5
+n = a > 0
+
+print (n)
+
+n = a>=3
+print(n)
+
+n = a == 5
+
+print(n)
+
+n = a!=5
+print(n)
+
+n = 0<a<10
+
+print(n)
+
---
+++ operators 3.py
@@ -0,0 +1,7 @@
+a = True and True
+print(a)
+
+n = 10>3 and 1 ==2
+print(n)
+
+# 산술 > 관계 > 논리 순서로 우선
