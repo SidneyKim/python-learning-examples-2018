6d04618b677b9acb64e46c808094168e88e433f4
Sidney Kim <wz0627@gmail.com>
2018-04-03 14:14:20 +0900

    after quiz 2

---
+++ operator 4.py
@@ -0,0 +1,5 @@
+n = 5
+rst = 100 if n>0 else 200
+
+print(rst)
+
--- operators 3.py
+++ operators 3.py
@@ -5,3 +5,13 @@ n = 10>3 and 1 ==2
 print(n)
 
 # 산술 > 관계 > 논리 순서로 우선
+
+
+n = False or True
+print(n)
+
+n = 10>3 or 10 ==2
+print(n)
+
+n = not 10>3
+print(n)
