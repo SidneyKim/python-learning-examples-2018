f030d0315c5b6bc2e38a53d40593ed66b6a2850c
Sidney Kim <wz0627@gmail.com>
2018-04-03 14:28:59 +0900

    after break time 3

---
+++ control stat 1.py
@@ -0,0 +1,28 @@
+if False:
+ print('hello')
+ print('korea')
+
+elif None:
+     print('x')
+
+elif 0:
+    print('x1')
+
+elif 0.0000000000000000:
+        print('x2')
+
+elif (0):
+    print('x3')
+
+elif [0]:
+    print('x5')
+
+elif {0}:
+    print('x4')
+
+
+else:
+ print('a')
+ print('aa')
+    
+print('python')
